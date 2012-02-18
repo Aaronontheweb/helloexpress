9fe571f9e01648ce47d5b28a2c4443f3ca81ae42
Aaron Stannard <aaron@aaronstannard.com>
2012-02-18 14:25:11 -0800

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-
 /**
  * Module dependencies.
  */
@@ -30,6 +29,5 @@ app.configure('production', function(){
 // Routes
 
 app.get('/', routes.index);
-
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
